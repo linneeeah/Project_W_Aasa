f8cb15567600031b1b65a6c12352342626f6b932
linneeeah <72727810+linneeeah@users.noreply.github.com>
2021-08-24 13:55:01 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -17,7 +17,7 @@
 ### -- set the email address -- 
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
-#BSUB -u aasa.feragen@gmail.com
+#BSUB -u s175247@student.dtu.dk
 ### -- send notification at start -- 
 #BSUB -B 
 ### -- send notification at completion -- 
